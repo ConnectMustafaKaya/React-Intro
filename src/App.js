bf7d2f618471a6b409b99a01324b5e57c74857ef
Mustafa Kaya <mustafa.kaya@setur.com.tr>
2022-01-15 16:18:28 +0300

    Added first component. Deleted Some src.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CategoryList from "./CategoryList";
+import Navi from "./Navi";
+import ProductList from "./ProductList";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome React.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <Navi/>
+       <CategoryList/>
+       <ProductList/>
     </div>
   );
 }
