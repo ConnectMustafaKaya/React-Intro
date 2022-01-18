ee1cb470a27c964b32ae7b65bd92d9c198eb6975
Mustafa Kaya <mustafa.kaya@setur.com.tr>
2022-01-18 21:32:04 +0300

    Working with events

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -6,6 +6,11 @@ export default class CategoryList extends Component {
       { categoryId: 1, categoryName: "Beverages" },
       { categoryId: 2, categoryName: "Condiments" },
     ],
+    currentCategory: "",
+  };
+
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
   };
 
   render() {
@@ -14,9 +19,15 @@ export default class CategoryList extends Component {
         <h3>{this.props.info.title}</h3>
         <ListGroup>
           {this.state.categories.map((category) => (
-            <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem>
+            <ListGroupItem
+              onClick={() => this.changeCategory(category)}
+              key={category.categoryId}
+            >
+              {category.categoryName}
+            </ListGroupItem>
           ))}
         </ListGroup>
+        <h4>{this.state.currentCategory}</h4>
       </div>
     );
   }
