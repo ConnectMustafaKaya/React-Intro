611a5b543ba6e5e7dd383d4d2739475809bdedf0
Mustafa Kaya <mustafa.kaya@setur.com.tr>
2022-01-18 22:00:32 +0300

    Sending data and event the other component.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import CategoryList from "./CategoryList";
 import Navi from "./Navi";
 import ProductList from "./ProductList";
-import { Container, Row,Col } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 
-function App() {
+export default class App extends Component {
+  state = { currentCategory: "" }
 
-  let productInfo={title:"Product List",smth:"Something"}
-  let categoryInfo={title:"Category List"}
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+  };
 
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi />
-        </Row>
-        <Row>
-        <Col>
-        <CategoryList info={categoryInfo} />
-        </Col>
-         <Col>
-         <ProductList info={productInfo} />
-         </Col>        
-        </Row>
-      </Container>
-    </div>
-  );
+  render() {
+    let productInfo = { title: "Product List", smth: "Something" };
+    let categoryInfo = { title: "Category List" };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col>
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              />
+            </Col>
+            <Col>
+              <ProductList currentCategory={this.state.currentCategory} info={productInfo} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/ProductList.js
+++ src/ProductList.js
@@ -11,7 +11,9 @@ export default class ProductList extends Component {
   render() {
     return (
       <div>
-        <h3>{this.props.info.title}</h3>
+        <h3>
+          {this.props.info.title} - {this.props.currentCategory}
+        </h3>
         <ListGroup>
           {this.state.products.map((product) => (
             <ListGroupItem key={product.productId}>
