5c91ee00e963a73afd61765464eb44b0a7a0e156
Mustafa Kaya <mustafa.kaya@setur.com.tr>
2022-02-26 14:28:35 +0300

    react-router-dom added.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'alertifyjs/build/css/alertify.min.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "alertifyjs/build/css/alertify.min.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ 
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
